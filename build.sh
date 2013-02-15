93d303f90590efff636432fceb61d17adafb3a54
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-15 22:42:07 +1000

    build editor not index

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ browserify css.js -o static/css-colab.js --debug
   echo '<!DOCTYPE HTML><html><body></body><script>'
   browserify client.js --debug --exports require || exit 1
   echo '</script></html>'
-} > static/index.html
+} > static/editor.html
 
